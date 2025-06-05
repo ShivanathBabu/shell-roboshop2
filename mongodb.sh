f746722e4fd6ee99a6216b320177399f0bdf7b89
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-05 22:27:49 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+
+Logs_folder="/var/log/roboshop-log"
+script_name=$( echo $0 | cut -d "." -f1)
+Log_file="$Logs_folder/$script_name.log"
+script_dir=$PWD
+
+mkdir -p $Logs_folder
+
+if [ $userid -ne 0 ]
+then
+echo -e "$r error: please run with root access $n" | tee -a $Log_file
+exit 1
+else
+echo -e "$g you are in root access $n" | tee -a $Log_file
+fi
+
+validate(){
+if [ $1 -eq 0 ]
+then
+ echo -e "$2.. $g success $n" | tee -a $Log_file
+ else
+ echo -e "$2... $r failure $n" | tee -a $Log_file
+ exit 1
+ fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
+validate $? "Copying Mongodb repo"
+
+dnf install mongodb-org-y
+validate $? "install mongodb"
+
+systemctl enable mongod
+validate $? "Enabling MongoDB"
+
+systemctl start mongod
+validate $? "starting mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+validate $? "Editing Mongod config file "
+
+systemctl restart mongod 
+validate  $? "Restarting MongoDB"
