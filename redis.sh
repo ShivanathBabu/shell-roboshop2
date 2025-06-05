9f437900a3a699d6d9436081ef56d2f6e05d842a
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-05 23:44:45 +0530

    roboshop

---
+++ redis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+
+Logs_folder="/var/log/roboshop-log"
+script_name=$( echo $0 | cut -d "." -f1)
+Log_file="$Logs_folder/$script_name.log"
+script_dir=$PWD
+
+mkdir -p $Logs_folder
+
+if [ $userid -ne 0 ]
+then
+echo -e "$r error: please run with root access $n" | tee -a $Log_file
+exit 1
+else
+echo -e "$g you are in root access $n" | tee -a $Log_file
+fi
+
+validate(){
+if [ $1 -eq 0 ]
+then
+ echo -e "$2.. $g success $n" | tee -a $Log_file
+ else
+ echo -e "$2... $r failure $n" | tee -a $Log_file
+ exit 1
+ fi
+}
+
+dnf module disable redis -y &>>$Log_file
+validate $? "disable default redis"
+
+dnf module enable redis :7 -y &>>$Log_file
+validate $? "enabling redis"
+
+dnf install redis -y &>>$Log_file
+validate $? "installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+validate $? "editing redis.conf"
+
+systemctl enable redis  &>>$Log_file
+validate $? "enable redis"
+
+systemctl start redis &>>$Log_file
+validate $? "start redis"
+
---
+++ user.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+
+Logs_folder="/var/log/roboshop-log"
+script_name=$( echo $0 | cut -d "." -f1)
+Log_file="$Logs_folder/$script_name.log"
+script_dir=$PWD
+
+mkdir -p $Logs_folder
+
+if [ $userid -ne 0 ]
+then
+echo -e "$r error: please run with root access $n" | tee -a $Log_file
+exit 1
+else
+echo -e "$g you are in root access $n" | tee -a $Log_file
+fi
+
+validate(){
+if [ $1 -eq 0 ]
+then
+ echo -e "$2.. $g success $n" | tee -a $Log_file
+ else
+ echo -e "$2... $r failure $n" | tee -a $Log_file
+ exit 1
+ fi
+}
+
+dnf module disable nodejs -y
+validate $? "disable nodejs"
+
+dnf module enable nodejs:20 -y
+validate $? "enable nodejs"
+
+dnf install nodejs -y
+validate $? "install nodejs"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+  validate $? "creating roboshop user"
+  else
+  echo -e " already user exists $y skipping $n"
+  fi
+
+rm -rf /app/*
+cd /app
+unzip /tmp/user.zip
+validate $? "downloading userzip file"
+
+cp $script_dir/user.service /etc/systemd/system/user.service
+validate $? "copying user.service"
+
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+validate $? "starting user"
+
