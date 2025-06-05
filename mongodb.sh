3ea836b6810db02638f5aec51bf5858a5a3239d3
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-05 22:42:34 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -31,7 +31,7 @@ then
  fi
 }
 
-cp mogo.repo /etc/yum.repos.d/mongodb.repo
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
 validate $? "Copying Mongodb repo"
 
 dnf install mongodb-org -y  &>>$Log_file
